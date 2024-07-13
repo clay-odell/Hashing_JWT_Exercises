6f800bf13c6fff4d2431a358fbf1e831e4445b7d
Clay O'Dell <clay.odell.music@gmail.com>
2024-07-12 19:14:42 -0500

    added env information to config.js

--- config.js
+++ config.js
@@ -3,10 +3,11 @@
 // read .env files and make environmental variables
 
 require("dotenv").config();
-
+const DB_USER = process.env.DB_USERNAME;
+const DB_PASSWORD = process.env.DB_PASSWORD;
 const DB_URI = (process.env.NODE_ENV === "test")
-  ? "postgresql:///messagely_test"
-  : "postgresql:///messagely";
+  ? `postgresql://${DB_USER}:${DB_PASSWORD}@localhost/messagely_test`
+  : `postgresql://${DB_USER}:${DB_PASSWORD}@localhost/messagely`;
 
 const SECRET_KEY = process.env.SECRET_KEY || "secret";
 
