c046ed8dd30f08ff4dce9e7689b61fc3619f3265
Clay O'Dell <clay.odell.music@gmail.com>
2024-07-13 18:05:51 -0500

    fixed an error on my auth.js route

--- routes/auth.js
+++ routes/auth.js
@@ -19,7 +19,7 @@ router.post('/login', async function(req, res, next) {
             User.updateLoginTimestamp(username);
             return res.json({ token });
         } else {
-            throw new ExpressError("Invalid username/password", 404);
+            throw new ExpressError("Invalid username/password", 400);
         }
     } catch (err) {
         return next(err);
