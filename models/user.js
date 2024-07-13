1d4183d6e21e0086910f44852c3a14303738d184
Clay O'Dell <clay.odell.music@gmail.com>
2024-07-13 16:08:13 -0500

    refactored User model to pass all tests

--- models/user.js
+++ models/user.js
@@ -4,6 +4,7 @@ const db = require("../db");
 const bcrypt = require("bcrypt");
 const { BCRYPT_WORK_FACTOR } = require("../config");
 const ExpressError = require("../expressError");
+const Message = require("./message");
 /** User of the site. */
 
 class User {
@@ -14,11 +15,12 @@ class User {
   static async register({ username, password, first_name, last_name, phone }) {
     const hashedPassword = await bcrypt.hash(password, BCRYPT_WORK_FACTOR);
     const result = await db.query(
-      `INSERT INTO users (username, password, first_name, last_name, phone)
-      VALUES ($1, $2, $3, $4, $5),
+      `INSERT INTO users (username, password, first_name, last_name, phone, join_at)
+      VALUES ($1, $2, $3, $4, $5, CURRENT_TIMESTAMP)
       RETURNING username, password, first_name, last_name, phone`,
       [username, hashedPassword, first_name, last_name, phone]
     );
+    await User.updateLoginTimestamp(username);
     return result.rows[0];
   }
 
@@ -81,8 +83,7 @@ class User {
     try {
       const result = await db.query(
         `SELECT username, first_name, last_name, phone, join_at, last_login_at FROM users
-        WHERE username = $1,
-        `,
+        WHERE username = $1`,
         [username]
       );
       if (result.rows.length === 0) {
@@ -102,8 +103,34 @@ class User {
    *   {username, first_name, last_name, phone}
    */
 
-  static async messagesFrom(username) {}
-
+  static async messagesFrom(username) {
+    try {
+      const results = await db.query(
+        `SELECT m.id, m.to_username, m.body, m.sent_at, m.read_at, u.username, u.first_name, u.last_name, u.phone
+        FROM messages AS m
+        JOIN users AS u ON m.to_username = u.username
+        WHERE m.from_username = $1`,
+        [username]
+      );
+      return results.rows.map((row) => ({
+        id: row.id,
+        to_user: {
+          username: row.username,
+          first_name: row.first_name,
+          last_name: row.last_name,
+          phone: row.phone,
+        },
+        body: row.body,
+        sent_at: row.sent_at,
+        read_at: row.read_at,
+      }));
+    } catch (err) {
+      throw new ExpressError(
+        "There was a problem retrieving the messages.",
+        500
+      );
+    }
+  }
   /** Return messages to this user.
    *
    * [{id, from_user, body, sent_at, read_at}]
@@ -112,7 +139,36 @@ class User {
    *   {username, first_name, last_name, phone}
    */
 
-  static async messagesTo(username) {}
+  static async messagesTo(username) {
+    try {
+      const results = await db.query(
+        `SELECT m.id, m.body, m.sent_at, m.read_at,
+              u.username, u.first_name, u.last_name, u.phone
+       FROM messages AS m
+       JOIN users AS u ON m.from_username = u.username
+       WHERE m.to_username = $1`,
+        [username]
+      );
+
+      return results.rows.map((row) => ({
+        id: row.id,
+        from_user: {
+          username: row.username,
+          first_name: row.first_name,
+          last_name: row.last_name,
+          phone: row.phone,
+        },
+        body: row.body,
+        sent_at: row.sent_at,
+        read_at: row.read_at,
+      }));
+    } catch (err) {
+      throw new ExpressError(
+        "There was a problem with retrieving the messages.",
+        500
+      );
+    }
+  }
 }
 
 module.exports = User;
